5b0cee288017f1206114e46a5d02d384caf6e219
mgbams <cmgbams@gmail.com>
2021-01-02 19:59:16 +0100

    accordion logic

--- src/components/AccordionLists.js
+++ src/components/AccordionLists.js
@@ -1,20 +1,44 @@
 import React, { useState } from "react";
 import SingleAccordion from "./SingleAccordion";
+import data from "../data";
 
 const AccordionLists = () => {
   const [isAccordionOpen, setIsAccordionOpen] = useState(false);
+  const [question, setQuestion] = useState(data);
+  const [showAccordion, setShowAccordion] = useState(data[0]);
 
-  const toggleAccordion = () => {
-    console.log("show");
+  const toggleAccordion = (id) => {
     setIsAccordionOpen(!isAccordionOpen);
+    console.log(isAccordionOpen);
+    for (let k = 0; k < question.length; k++) {
+      if (question[k].id === id) {
+        setShowAccordion(question[k]);
+        if (showAccordion.id === id) {
+          setIsAccordionOpen(!isAccordionOpen);
+        }
+      }
+    }
   };
 
   return (
-    <div>
-      <SingleAccordion
-        isAccordionOpen={isAccordionOpen}
-        toggleAccordion={toggleAccordion}
-      />
+    <div className="accordion-lists-wrapper">
+      <div id="questions-and-answers-title">
+        <h2>Questions And Answers About Login</h2>
+      </div>
+      <div id="reviews-container">
+        {data.map((review) => {
+          let { id } = review;
+          return (
+            <SingleAccordion
+              key={id}
+              isAccordionOpen={isAccordionOpen}
+              toggleAccordion={() => toggleAccordion(id)}
+              {...review}
+              showAccordion={showAccordion}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/components/SingleAccordion.js
+++ src/components/SingleAccordion.js
@@ -1,34 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const SingleAccordion = ({ isAccordionOpen, toggleAccordion }) => {
+const SingleAccordion = ({
+  isAccordionOpen,
+  toggleAccordion,
+  showAccordion,
+  id,
+  title,
+  info,
+}) => {
   return (
     <section id="main-container">
-      <div style={{ width: "30%" }}>
-        <h2>Questions And Answers About Login</h2>
-      </div>
       <div id="accordion-wrapper">
         <div className="accordion-inner-wrapper">
           <div className="title-and-button">
             <h3>
-              <strong>Question to be answered and answered?</strong>
+              <strong>{title}</strong>
             </h3>
           </div>
           <div className="button-wrapper">
             <button id="button" onClick={toggleAccordion}>
-              {isAccordionOpen ? "-" : "+"}
+              {isAccordionOpen && showAccordion.id === id ? "-" : "+"}
             </button>
           </div>
         </div>
 
-        <div style={{ width: "32vw" }}>
-          {isAccordionOpen && (
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo,
-              neque voluptate quidem dolores repellendus voluptatum dolorum
-              reprehenderit, saepe dicta similique architecto, facilis maxime
-              illo doloremque.
-            </p>
+        <div style={{ width: "30vw" }}>
+          {showAccordion.id === id && isAccordionOpen ? (
+            <p>{info}</p>
+          ) : (
+            !isAccordionOpen
           )}
         </div>
       </div>
